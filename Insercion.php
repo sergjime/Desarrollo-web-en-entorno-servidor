921720f7483a85a8e97a0884935687e52f74eda2
Sergio <32309472+sergjime@users.noreply.github.com>
2018-11-02 09:41:44 +0100

    Add files via upload

---
+++ Insercion.php
@@ -0,0 +1,17 @@
+<?php
+// ------------------------------------------------------------------------------ MÉTODO INSERCIÓN DIRECTA ------------------------------------------------------------------------------
+function insercionDirecta($array,$num_array){ // funcion insercionDirecta que recibe dos parámetros (el array y la longitud del array)
+    for ($i = 1; $i < $num_array; $i++){ 
+                $posicion = $array[$i];
+                $j = $i - 1;
+                while ($j >= 0 && $array[$j] > $posicion){ // ciclo que mueve la posición posterior de la array si esta es mas peque�a que la anterior
+                $array[$j + 1] = $array[$j];
+                $j--;
+                } 
+                $array[$j + 1] = $posicion;
+    }
+    for($i=0;$i<sizeof($array);$i++){
+        echo $array[$i]." ";
+    }
+}
+?>
